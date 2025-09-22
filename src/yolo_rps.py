c94e1143be94461e8830c45fa82398590d9dd15f
Daeyong Kim <youbinlove607@gmail.com>
2025-09-22 17:13:33 +0900

    .

--- src/yolo_rps.py
+++ src/yolo_rps.py
@@ -4,8 +4,13 @@ import os
 import json
 
 # TODO: 커스텀 모델 로드
+model_path = os.path.abspath("/home/intel/Study/yolov5/Assignment/d020rs-yolov5-rock-paper-scissors-Dae-Yong-Kim/src/rps_model.onnx")
+model = torch.hub.load("ultralytics/yolov5", "custom", path=model_path)
 
 # TODO: Label 로드
+label_path = os.path.abspath("/home/intel/Study/yolov5/Assignment/d020rs-yolov5-rock-paper-scissors-Dae-Yong-Kim/src/rps_model.names.json")
+with open(label_path, "r") as f:
+    label_dict = json.load(f)
 
 # Video capture
 cap = cv2.VideoCapture(0)
@@ -20,11 +25,16 @@ while True:
 
     # 추론 실행 (BGR -> RGB)
     rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+    # TODO: 카메라 입력의 크기(frame_h, frame_w)와 모델의 입력 크기(input_h, input_w) 구하기
+    frame_h, frame_w, _ = rgb_frame.shape
+    resized_h, resized_w = 640, 640
+
     # TODO: 추론 전 입력 크기 보정 (640x640)
+    rgb_frame = cv2.resize(rgb_frame, (resized_h, resized_w))
 
     results = model(rgb_frame)
 
-    # TODO: 카메라 입력의 크기(frame_h, frame_w)와 모델의 입력 크기(input_h, input_w) 구하기
 
     # Boudning box 그리기
     for i, obj in enumerate(results.xyxy[0]):
@@ -33,8 +43,11 @@ while True:
         conf = obj[4]
 
         # TODO: 인식된 정확도(confidence)와 클래스를 label로 구성
+        label = label_dict[str(cls)]
 
         # TODO: 출력 바운딩박스 크기 조절
+        y1 = y1 * frame_h // resized_h
+        y2 = y2 * frame_h // resized_h
 
         # OpenCV를 이용해서 해당 좌표에 사각형과 text를 출력
         cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
